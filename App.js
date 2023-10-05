ac5aa718f65c060a55570b0ac954cbd0d6c94790
asqarbek olimov <88034288+asqarbekolimov@users.noreply.github.com>
2023-10-05 11:45:31 +0500

    Navbar & Scroll view

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { NativeWindStyleSheet } from "nativewind";
-import Home from "./src/screens/home";
 import AppNavigations from "./src/navigations/app.navigations";
-import { TabNavigation } from "./src/navigations/tab.navigation";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
 });
 
 export default function App() {
-  return <TabNavigation />;
+  return <AppNavigations />;
 }
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Button, Image, ScrollView, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 
 export default function Home({ navigation }) {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-400">
-      <Text>Home!</Text>
-      <Button
-        title="GO to details"
-        onPress={() => navigation.navigate("Detailed")}
-      />
+    <View className="flex-1 bg-slate-950">
+      <SafeAreaView>
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-3">
+          <Image source={require("../../assets/logo.png")} />
+          <MagnifyingGlassIcon size={30} strokeWidth={2} color={"white"} />
+        </View>
+      </SafeAreaView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 20 }}
+      ></ScrollView>
     </View>
   );
 }
